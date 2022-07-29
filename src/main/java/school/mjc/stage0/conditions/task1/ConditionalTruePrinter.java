c10e61d774a88ac4d9ab95d6087a3a3190e8f0ec
komronz <pandacoder0820@gmail.com>
2022-07-29 21:58:35 +0500

    module4Task1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,7 +2,8 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
-        if (1>0) System.out.println(true);
-        else System.out.println(false);
+        if (true) {
+            System.out.println(true);
+        }
     }
 }
